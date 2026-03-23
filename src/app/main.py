23312b0329a5ea984e38bc252610b22affe36168
himelinakai <honukamaliiwahine@icloud.com>
2026-03-22 22:50:37 -0700

    Updated main.py imports and models/init.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from app.core.database import Base, engine
+# from app.models import Favorite, History, Restaurant, User
+from app.models import Restaurant
+
+Base.metadata.create_all(bind=engine) 
 
 # This variable NAME must be "app" because that is what your command is looking for
 app = FastAPI() 
