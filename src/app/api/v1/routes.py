77fb6501c694e31c233cff6423fb0f3247d5a975
himelinakai <honukamaliiwahine@icloud.com>
2026-03-23 01:33:07 -0700

    Define restaurant route

---
+++ src/app/api/v1/routes.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from app.routes import restaurant
+# from app.routes import restaurant, user, history, favorites, randomizer
+
+api_router = APIRouter(prefix="/api/v1")
+
+api_router.include_router(restaurant.api_router)
+# api_router.include_router(user.api_router)
+# api_router.include_router(history.api_router)
+# api_router.include_router(favorites.api_router)
+# api_router.include_router(randomizer.api_router)
