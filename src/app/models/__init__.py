2b55738ea8e116f84f1af0c82e00e4b2ddb61cbf
himelinakai <honukamaliiwahine@icloud.com>
2026-03-22 22:37:28 -0700

    empty models init

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,4 +1,4 @@
 # from app.models.user import User
-from app.models.restaurant import Restaurant
+# from app.models.restaurant import Restaurant
 # from app.models.favorites import Favorite
 # from app.models.history import History
