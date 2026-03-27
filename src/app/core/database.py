b3acab9d2d651cf58c815573e6d1fb116ab7f5fd
Valerie Joy Pinto <143357601+ValerieJoyPinto@users.noreply.github.com>
2026-03-27 15:51:57 -0700

    Save my database config

--- src/app/core/database.py
+++ src/app/core/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from app.core.settings import settings
 
 engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
-=======
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,7 +14,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 engine = create_engine(os.getenv("DATABASE_URL"))
->>>>>>> f9aaf906a3af3a9c534bd271492ec03c02340716
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
