a03efcf8c93d4dbb89c7cadb7dab828d601c66b5
Kaitlin <kbituen0493@sdsu.edu>
2026-05-03 23:35:32 -0700

    add placeholder data

--- frontend/src/app/history/page.tsx
+++ frontend/src/app/history/page.tsx
@@ -1,5 +1,37 @@
 // src/app/history/page.tsx
 
+//Placeholders until backend/auth is connected
+//Newest randomizer results appear first in array
+const historyItems = [
+  {
+    name: "Los Primos Mexican Food",
+    emoji: "🌯",
+    rating: 4.5,
+    reviews: 689,
+    price: "$",
+    category: "Mexican",
+    onCampus: false,
+  },
+  {
+    name: "Oggi's Pizza",
+    emoji: "🍕",
+    rating: 4.2,
+    reviews: 534,
+    price: "$$",
+    category: "Pizza",
+    onCampus: false,
+  },
+  {
+    name: "Chipotle",
+    emoji: "🌯",
+    rating: 4.3,
+    reviews: 2341,
+    price: "$$",
+    category: "Mexican",
+    onCampus: true,
+  },
+];
+
 //based on Next.js App Router page structure
 export default function HistoryPage(){
     return(
