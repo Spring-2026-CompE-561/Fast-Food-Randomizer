c5b5082de1821d5b903a6b4560cbdace802a464b
himelinakai <honukamaliiwahine@icloud.com>
2026-05-04 00:12:52 -0700

    Add protected route logic

---
+++ frontend/src/app/favorites/page.tsx
@@ -0,0 +1,9 @@
+import ProtectedRoute from "@/components/ProtectedRoute";
+
+export default function FavoritesPage() {
+  return (
+    <ProtectedRoute>
+      <div>Favorites Page (placeholder)</div>
+    </ProtectedRoute>
+  );
+}
